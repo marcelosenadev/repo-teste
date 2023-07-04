76bc549373d569b315d5677e3a852f026168dc72
Marcelo Sena Dev <marcelosenadev1@gmail.com>
2023-07-03 21:40:11 -0300

    Alteração no Controller

--- src/main/java/com/sena/primeiro/controller/PessoaController.java
+++ src/main/java/com/sena/primeiro/controller/PessoaController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/pessoa")
 public class PessoaController {
-	
 	@GetMapping
 	public String retorno() {
-		
 		return "hello";
 	}
-
 }
